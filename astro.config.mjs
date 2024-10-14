499a827f0df3e81ddff8547f0362fa956689e439
Ivan Starikov <vanya.st.ov@gmail.com>
2024-10-14 22:08:02 +0300

    Add main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,2 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()]
-});
+import { defineConfig } from "astro/config";
+export default defineConfig({});
